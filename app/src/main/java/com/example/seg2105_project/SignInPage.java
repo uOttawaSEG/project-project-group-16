7427c9d56719e148b68ecc4b289f0eceb37ae362
Lili Rose T <145508020+LiliRT@users.noreply.github.com>
2024-09-26 16:26:25 -0400

    Added Sign In button on main page, and created an Intent for this Sign In Button

--- app/src/main/java/com/example/seg2105_project/SignInPage.java
+++ app/src/main/java/com/example/seg2105_project/SignInPage.java
@@ -8,37 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import android.content.Intent;
-import android.view.View;
-import android.widget.Button;
-
-public class MainActivity extends AppCompatActivity {
-
-    private Button signInButton;
+public class SignInPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_in_page);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        signInButton = findViewById(R.id.signInButton);
-
-        signInButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, SignInPage.class);
-                startActivity(intent);
-            }
-        });
-
-
     }
-
-
-
 }
