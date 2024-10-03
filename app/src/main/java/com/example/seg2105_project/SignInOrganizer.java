8de0d7691b59c70f342285d4bf5ad1482575ff84
Lili Rose T <145508020+LiliRT@users.noreply.github.com>
2024-10-03 17:05:42 -0400

    Creating ArrayLists to replace the databases

--- app/src/main/java/com/example/seg2105_project/SignInOrganizer.java
+++ app/src/main/java/com/example/seg2105_project/SignInOrganizer.java
@@ -1,6 +1,9 @@
 package com.example.seg2105_project;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class SignInOrganizer extends AppCompatActivity {
+
+    private EditText firstName;
+    private EditText lastName;
+    private EditText emailAddress;
+    private EditText phoneNumber;
+    private EditText address;
+    private EditText password;
+
+    private Button submitButton;
+
+    private ArrayList userData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +36,30 @@ public class SignInOrganizer extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        firstName = findViewById(R.id.firstNameFieldOrganizer);
+        lastName = findViewById(R.id.lastNameFieldOrganizer);
+        emailAddress = findViewById(R.id.emailAddressFieldOrganizer);
+        phoneNumber = findViewById(R.id.phoneNumberOrganizer);
+        address = findViewById(R.id.addressFieldOrganizer);
+        password = findViewById(R.id.confirmPasswordOrganizer);
+
+
+        submitButton = findViewById(R.id.submitOrganizerButton);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                userData = new ArrayList(6);
+                userData.add(emailAddress);
+                userData.add(password);
+                userData.add(firstName);
+                userData.add(lastName);
+                userData.add(phoneNumber);
+                userData.add(address);
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/seg2105_project/SigninAttendee.java
+++ app/src/main/java/com/example/seg2105_project/SigninAttendee.java
@@ -1,6 +1,7 @@
 package com.example.seg2105_project;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,19 +9,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.util.HashMap;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import java.util.ArrayList;
 
 public class SigninAttendee extends AppCompatActivity {
 
-    private String firstName;
-    private String lastName;
-    private String emailAddress;
-    private Integer phoneNumber;
-    private String address;
-    private String password;
+    private EditText firstName;
+    private EditText lastName;
+    private EditText emailAddress;
+    private EditText phoneNumber;
+    private EditText address;
+    private EditText password;
 
-    private HashMap<String, String> attendeeFirstName;
+    private Button submitButton;
+
+    private ArrayList userData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,5 +39,29 @@ public class SigninAttendee extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        firstName = findViewById(R.id.firstNameFieldAttendee);
+        lastName = findViewById(R.id.lastNameFieldAttendee);
+        emailAddress = findViewById(R.id.emailAddressFieldAttendee);
+        phoneNumber = findViewById(R.id.phoneNumberAttendee);
+        address = findViewById(R.id.addressFieldAttendee);
+        password = findViewById(R.id.confirmPasswordAttendee);
+
+
+        submitButton = findViewById(R.id.submitAttendeeButton);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                userData = new ArrayList(6);
+                userData.add(emailAddress);
+                userData.add(password);
+                userData.add(firstName);
+                userData.add(lastName);
+                userData.add(phoneNumber);
+                userData.add(address);
+            }
+        });
+
     }
 }
