098772cede0f39663905587e5a025b9dad620276
Hiba <ahansalhibatollah@gmail.com>
2024-10-05 14:41:07 -0400

    Log Off functionality of returnnung to the main page added

--- app/src/main/java/com/example/seg2105_project/WelcomePage.java
+++ app/src/main/java/com/example/seg2105_project/WelcomePage.java
@@ -1,5 +1,6 @@
 package com.example.seg2105_project;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,12 +11,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import android.widget.Button;
+import android.widget.TextView;
 
 
 public class WelcomePage extends AppCompatActivity {
     private TextView welcomeMessage;
-    private Button LogOffButton;
+    private Button logOffButton;
     private ArrayList<String> userData;
 
 
@@ -31,19 +32,26 @@ public class WelcomePage extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        //initialize UI
         welcomeMessage=findViewById(R.id.WelcomeMessage);
         logOffButton=findViewById(R.id.logOffButton);
-        logOffButton.setOnClickListener(new View.OnClickListener() {
+        // Retrieve the userData passed from Signin Attendee
+        userData = getIntent().getStringArrayListExtra("userData");
+        if (userData != null && !userData.isEmpty()) {
+
+            // Use the data as needed (e.g., display the first name)
+            String firstName = userData.get(0);  // Assuming the first element is firstName
+            welcomeMessage.setText("Hello , " + firstName + "!");
+        }
+            // set up the log off click listener
+            logOffButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
-
-
-                //Redirect to the LogIn page
+                //Redirect to the LogIn of the MainActivity page
                 Intent intent = new Intent(WelcomePage.this, MainActivity.class);
-
+                startActivity(intent);  // Start the MainActivity
+                finish();  // Finish the current activity so the user cannot go back
             }
+            });
         }
     }
-}
