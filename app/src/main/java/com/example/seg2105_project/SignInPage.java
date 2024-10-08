8af374594c3acefe0fbe85700b6eaf03cb43d358
LinaElYacoubi <linaelyacoubi60@gmail.com>
2024-10-08 18:11:15 -0400

    added the two logInAs on Organizer/ an Attendee buttons

--- app/src/main/java/com/example/seg2105_project/SignInPage.java
+++ app/src/main/java/com/example/seg2105_project/SignInPage.java
@@ -1,6 +1,9 @@
 package com.example.seg2105_project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +13,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class SignInPage extends AppCompatActivity {
 
-
-
-
+    private Button signInAttendeeButton;
+    private Button signInOrganizerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +26,29 @@ public class SignInPage extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+
+        signInAttendeeButton = findViewById(R.id.signInAttendeeButton);
+
+        signInAttendeeButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(SignInPage.this, SigninAttendee.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+        signInOrganizerButton = findViewById(R.id.signInOrganizerButton);
+
+        signInOrganizerButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(SignInPage.this, SignInOrganizer.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
