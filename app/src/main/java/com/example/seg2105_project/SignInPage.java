a0cc6e2a76710cbd6bb0982eebd35b2f7e02527f
Alae_Kabir <kabiralae2006@gmail.com>
2024-10-14 15:03:58 -0400

    Field validation for SignInOrganizer

--- app/src/main/java/com/example/seg2105_project/SignInPage.java
+++ app/src/main/java/com/example/seg2105_project/SignInPage.java
@@ -11,6 +11,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.text.TextUtils;
+import android.util.Patterns;
+
 public class SignInPage extends AppCompatActivity {
 
     private Button signInAttendeeButton;
